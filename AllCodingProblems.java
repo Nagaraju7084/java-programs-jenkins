869caa3958cffcbe2c7ff1a1febdbe21339c9354
nagaraju_kailasa <nagaraju.kailasa123@gmail.com>
2023-09-18 16:40:25 +0530

    added findFirstNonRepeatedCharacterInAString() method
    add the below method
    findFirstNonRepeatedCharacterInAString()
    to find the non repeated character in a given String

--- AllCodingProblems.java
+++ AllCodingProblems.java
@@ -3,7 +3,19 @@ import java.util.Date;
 public class AllCodingProblems{
     public static void main(String[] args) {
         System.out.println(new Date());
-        finbonacci();
+        //finbonacci();
+        findFirstNonRepeatedCharacterInAString();
+
+    }
+
+    private static void findFirstNonRepeatedCharacterInAString() {
+        String str = "zzzzzbbbccccddehhhhiii";
+        for(int i=0; i<str.length()-1; i++){
+            int ch = str.charAt(i);
+            if(str.indexOf(ch) == str.lastIndexOf(ch)){
+                System.out.println("first non repeated character in a String is : "+ch);
+            }
+        }
     }
 
     private static void finbonacci() {
